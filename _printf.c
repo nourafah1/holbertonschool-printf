88265bf521d559e286954a358683f2eda740c5ed
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-04-18 19:15:14 +0000

    Handle - flag for non-custom conversion specifiers

--- _printf.c
+++ _printf.c
@@ -29,7 +29,7 @@ static int get_uint_len(unsigned long int n, int base)
 int _printf(const char *format, ...)
 {
 	int i = 0, count = 0;
-	int plus_flag, space_flag, hash_flag, zero_flag;
+	int plus_flag, space_flag, hash_flag, zero_flag, minus_flag;
 	int long_mod, short_mod;
 	int width, j, precision;
 	char *str, c;
@@ -48,13 +48,15 @@ int _printf(const char *format, ...)
 			space_flag = 0;
 			hash_flag = 0;
 			zero_flag = 0;
+			minus_flag = 0;
 			long_mod = 0;
 			short_mod = 0;
 			width = 0;
 			precision = -1;
 			i++;
 			while (format[i] == '+' || format[i] == ' ' ||
-				format[i] == '#' || format[i] == '0')
+				format[i] == '#' || format[i] == '0' ||
+				format[i] == '-')
 			{
 				if (format[i] == '+')
 					plus_flag = 1;
@@ -64,8 +66,12 @@ int _printf(const char *format, ...)
 					hash_flag = 1;
 				else if (format[i] == '0')
 					zero_flag = 1;
+				else if (format[i] == '-')
+					minus_flag = 1;
 				i++;
 			}
+			if (minus_flag)
+				zero_flag = 0;
 			if (format[i] == '*')
 			{
 				width = va_arg(args, int);
@@ -116,10 +122,19 @@ int _printf(const char *format, ...)
 			}
 			if (format[i] == 'c')
 			{
-				for (j = 1; j < width; j++)
-					add_to_buffer(buffer, &buf_index, ' ', &count);
 				c = va_arg(args, int);
-				add_to_buffer(buffer, &buf_index, c, &count);
+				if (minus_flag)
+				{
+					add_to_buffer(buffer, &buf_index, c, &count);
+					for (j = 1; j < width; j++)
+						add_to_buffer(buffer, &buf_index, ' ', &count);
+				}
+				else
+				{
+					for (j = 1; j < width; j++)
+						add_to_buffer(buffer, &buf_index, ' ', &count);
+					add_to_buffer(buffer, &buf_index, c, &count);
+				}
 			}
 			else if (format[i] == 's')
 			{
@@ -135,10 +150,20 @@ int _printf(const char *format, ...)
 					print_len = precision;
 				else
 					print_len = len;
-				for (j = print_len; j < width; j++)
-					add_to_buffer(buffer, &buf_index, ' ', &count);
-				for (j = 0; j < print_len; j++)
-					add_to_buffer(buffer, &buf_index, str[j], &count);
+				if (minus_flag)
+				{
+					for (j = 0; j < print_len; j++)
+						add_to_buffer(buffer, &buf_index, str[j], &count);
+					for (j = print_len; j < width; j++)
+						add_to_buffer(buffer, &buf_index, ' ', &count);
+				}
+				else
+				{
+					for (j = print_len; j < width; j++)
+						add_to_buffer(buffer, &buf_index, ' ', &count);
+					for (j = 0; j < print_len; j++)
+						add_to_buffer(buffer, &buf_index, str[j], &count);
+				}
 			}
 			else if (format[i] == 'S')
 			{
@@ -149,9 +174,18 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i] == '%')
 			{
-				for (j = 1; j < width; j++)
-					add_to_buffer(buffer, &buf_index, ' ', &count);
-				add_to_buffer(buffer, &buf_index, '%', &count);
+				if (minus_flag)
+				{
+					add_to_buffer(buffer, &buf_index, '%', &count);
+					for (j = 1; j < width; j++)
+						add_to_buffer(buffer, &buf_index, ' ', &count);
+				}
+				else
+				{
+					for (j = 1; j < width; j++)
+						add_to_buffer(buffer, &buf_index, ' ', &count);
+					add_to_buffer(buffer, &buf_index, '%', &count);
+				}
 			}
 			else if (format[i] == 'd' || format[i] == 'i')
 			{
@@ -160,7 +194,6 @@ int _printf(const char *format, ...)
 				int zero_pad = 0;
 				int pad_total;
 				long int tmp;
-				char pad_char = zero_flag ? '0' : ' ';
 
 				if (long_mod)
 					num = va_arg(args, long int);
@@ -193,7 +226,21 @@ int _printf(const char *format, ...)
 					pad_total = digits + zero_pad;
 					if (num < 0 || plus_flag || space_flag)
 						pad_total++;
-					if (zero_flag && precision == -1)
+					if (minus_flag)
+					{
+						if (num < 0)
+							add_to_buffer(buffer, &buf_index, '-', &count);
+						else if (plus_flag)
+							add_to_buffer(buffer, &buf_index, '+', &count);
+						else if (space_flag)
+							add_to_buffer(buffer, &buf_index, ' ', &count);
+						for (j = 0; j < zero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						print_number_buffer(tmp, buffer, &buf_index, &count);
+						for (j = pad_total; j < width; j++)
+							add_to_buffer(buffer, &buf_index, ' ', &count);
+					}
+					else if (zero_flag && precision == -1)
 					{
 						if (num < 0)
 							add_to_buffer(buffer, &buf_index, '-', &count);
@@ -203,21 +250,24 @@ int _printf(const char *format, ...)
 							add_to_buffer(buffer, &buf_index, ' ', &count);
 						for (j = pad_total; j < width; j++)
 							add_to_buffer(buffer, &buf_index, '0', &count);
+						for (j = 0; j < zero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						print_number_buffer(tmp, buffer, &buf_index, &count);
 					}
 					else
 					{
 						for (j = pad_total; j < width; j++)
-							add_to_buffer(buffer, &buf_index, pad_char, &count);
+							add_to_buffer(buffer, &buf_index, ' ', &count);
 						if (num < 0)
 							add_to_buffer(buffer, &buf_index, '-', &count);
 						else if (plus_flag)
 							add_to_buffer(buffer, &buf_index, '+', &count);
 						else if (space_flag)
 							add_to_buffer(buffer, &buf_index, ' ', &count);
+						for (j = 0; j < zero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						print_number_buffer(tmp, buffer, &buf_index, &count);
 					}
-					for (j = 0; j < zero_pad; j++)
-						add_to_buffer(buffer, &buf_index, '0', &count);
-					print_number_buffer(tmp, buffer, &buf_index, &count);
 				}
 			}
 			else if (format[i] == 'b')
@@ -229,7 +279,6 @@ int _printf(const char *format, ...)
 				int udigits = 0;
 				int uzero_pad = 0;
 				int upad_total;
-				char upad_char = zero_flag ? '0' : ' ';
 
 				if (long_mod)
 					unum = va_arg(args, unsigned long int);
@@ -248,19 +297,40 @@ int _printf(const char *format, ...)
 					if (precision > udigits)
 						uzero_pad = precision - udigits;
 					upad_total = udigits + uzero_pad;
-					for (j = upad_total; j < width; j++)
-						add_to_buffer(buffer, &buf_index, upad_char, &count);
-					for (j = 0; j < uzero_pad; j++)
-						add_to_buffer(buffer, &buf_index, '0', &count);
-					if (long_mod)
-						print_unsigned_long_buffer(unum,
-							buffer, &buf_index, &count);
-					else if (short_mod)
-						print_unsigned_buffer((unsigned short int)unum,
-							buffer, &buf_index, &count);
+					if (minus_flag)
+					{
+						for (j = 0; j < uzero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						if (long_mod)
+							print_unsigned_long_buffer(unum,
+								buffer, &buf_index, &count);
+						else if (short_mod)
+							print_unsigned_buffer((unsigned short int)unum,
+								buffer, &buf_index, &count);
+						else
+							print_unsigned_buffer((unsigned int)unum,
+								buffer, &buf_index, &count);
+						for (j = upad_total; j < width; j++)
+							add_to_buffer(buffer, &buf_index, ' ', &count);
+					}
 					else
-						print_unsigned_buffer((unsigned int)unum,
-							buffer, &buf_index, &count);
+					{
+						char upad_char = zero_flag ? '0' : ' ';
+
+						for (j = upad_total; j < width; j++)
+							add_to_buffer(buffer, &buf_index, upad_char, &count);
+						for (j = 0; j < uzero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						if (long_mod)
+							print_unsigned_long_buffer(unum,
+								buffer, &buf_index, &count);
+						else if (short_mod)
+							print_unsigned_buffer((unsigned short int)unum,
+								buffer, &buf_index, &count);
+						else
+							print_unsigned_buffer((unsigned int)unum,
+								buffer, &buf_index, &count);
+					}
 				}
 			}
 			else if (format[i] == 'o')
@@ -269,7 +339,6 @@ int _printf(const char *format, ...)
 				int odigits = 0;
 				int ozero_pad = 0;
 				int opad_total;
-				char opad_char = zero_flag ? '0' : ' ';
 
 				if (long_mod)
 					onum = va_arg(args, unsigned long int);
@@ -290,18 +359,40 @@ int _printf(const char *format, ...)
 					if (hash_flag && onum != 0)
 						ozero_pad++;
 					opad_total = odigits + ozero_pad;
-					for (j = opad_total; j < width; j++)
-						add_to_buffer(buffer, &buf_index, opad_char, &count);
-					for (j = 0; j < ozero_pad; j++)
-						add_to_buffer(buffer, &buf_index, '0', &count);
-					if (long_mod)
-						print_octal_long_buffer(onum, buffer, &buf_index, &count);
-					else if (short_mod)
-						print_octal_buffer((unsigned short int)onum,
-							buffer, &buf_index, &count);
+					if (minus_flag)
+					{
+						for (j = 0; j < ozero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						if (long_mod)
+							print_octal_long_buffer(onum,
+								buffer, &buf_index, &count);
+						else if (short_mod)
+							print_octal_buffer((unsigned short int)onum,
+								buffer, &buf_index, &count);
+						else
+							print_octal_buffer((unsigned int)onum,
+								buffer, &buf_index, &count);
+						for (j = opad_total; j < width; j++)
+							add_to_buffer(buffer, &buf_index, ' ', &count);
+					}
 					else
-						print_octal_buffer((unsigned int)onum,
-							buffer, &buf_index, &count);
+					{
+						char opad_char = zero_flag ? '0' : ' ';
+
+						for (j = opad_total; j < width; j++)
+							add_to_buffer(buffer, &buf_index, opad_char, &count);
+						for (j = 0; j < ozero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						if (long_mod)
+							print_octal_long_buffer(onum,
+								buffer, &buf_index, &count);
+						else if (short_mod)
+							print_octal_buffer((unsigned short int)onum,
+								buffer, &buf_index, &count);
+						else
+							print_octal_buffer((unsigned int)onum,
+								buffer, &buf_index, &count);
+					}
 				}
 			}
 			else if (format[i] == 'x' || format[i] == 'X')
@@ -311,7 +402,6 @@ int _printf(const char *format, ...)
 				int xzero_pad = 0;
 				int xpad_total;
 				int uppercase = (format[i] == 'X');
-				char xpad_char = zero_flag ? '0' : ' ';
 
 				if (long_mod)
 					xnum = va_arg(args, unsigned long int);
@@ -332,7 +422,7 @@ int _printf(const char *format, ...)
 					xpad_total = xdigits + xzero_pad;
 					if (hash_flag && xnum != 0)
 						xpad_total += 2;
-					if (zero_flag && precision == -1)
+					if (minus_flag)
 					{
 						if (hash_flag && xnum != 0)
 						{
@@ -340,31 +430,58 @@ int _printf(const char *format, ...)
 							add_to_buffer(buffer, &buf_index,
 								uppercase ? 'X' : 'x', &count);
 						}
-						for (j = xpad_total; j < width; j++)
+						for (j = 0; j < xzero_pad; j++)
 							add_to_buffer(buffer, &buf_index, '0', &count);
+						if (long_mod)
+							print_hex_long_buffer(xnum, uppercase,
+								buffer, &buf_index, &count);
+						else if (short_mod)
+							print_hash_hex((unsigned short int)xnum,
+								0, uppercase, buffer, &buf_index, &count);
+						else
+							print_hash_hex((unsigned int)xnum,
+								0, uppercase, buffer, &buf_index, &count);
+						for (j = xpad_total; j < width; j++)
+							add_to_buffer(buffer, &buf_index, ' ', &count);
 					}
 					else
 					{
-						for (j = xpad_total; j < width; j++)
-							add_to_buffer(buffer, &buf_index, xpad_char, &count);
-						if (hash_flag && xnum != 0)
+						char xpad_char = zero_flag ? '0' : ' ';
+
+						if (zero_flag && precision == -1)
 						{
-							add_to_buffer(buffer, &buf_index, '0', &count);
-							add_to_buffer(buffer, &buf_index,
-								uppercase ? 'X' : 'x', &count);
+							if (hash_flag && xnum != 0)
+							{
+								add_to_buffer(buffer, &buf_index, '0', &count);
+								add_to_buffer(buffer, &buf_index,
+									uppercase ? 'X' : 'x', &count);
+							}
+							for (j = xpad_total; j < width; j++)
+								add_to_buffer(buffer, &buf_index, '0', &count);
 						}
+						else
+						{
+							for (j = xpad_total; j < width; j++)
+								add_to_buffer(buffer, &buf_index, xpad_char, &count);
+							if (hash_flag && xnum != 0)
+							{
+								add_to_buffer(buffer, &buf_index, '0', &count);
+								add_to_buffer(buffer, &buf_index,
+									uppercase ? 'X' : 'x', &count);
+							}
+						}
+						for (j = 0; j < xzero_pad; j++)
+							add_to_buffer(buffer, &buf_index, '0', &count);
+						if (long_mod)
+							print_hex_long_buffer(xnum, uppercase,
+								buffer, &buf_index, &count);
+						else if (short_mod)
+							print_hash_hex((unsigned short int)xnum,
+								0, uppercase, buffer, &buf_index, &count);
+						else
+							print_hash_hex((unsigned int)xnum,
+								0, uppercase, buffer, &buf_index, &count);
 					}
-					for (j = 0; j < xzero_pad; j++)
-						add_to_buffer(buffer, &buf_index, '0', &count);
-					if (long_mod)
-						print_hex_long_buffer(xnum, uppercase,
-							buffer, &buf_index, &count);
-					else if (short_mod)
-						print_hash_hex((unsigned short int)xnum,
-							0, uppercase, buffer, &buf_index, &count);
-					else
-						print_hash_hex((unsigned int)xnum,
-							0, uppercase, buffer, &buf_index, &count);
 				}
 			}
 			else if (format[i] == 'p')
